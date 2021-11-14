1fa30a977863ffeb629eadc5a453abcec97c51e0
aditya chaudhari <aditya.ch@keka.com>
2021-11-14 22:28:13 +0530

    final-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrushesComponent } from './brushes/brushes.component';
+import { CleanserComponent } from './cleanser/cleanser.component';
 import { DashboardCarouselComponent } from './dashboard-carousel/dashboard-carousel.component';
+import { HairCareComponent } from './hair-care/hair-care.component';
 import { HeaderComponent } from './header/header.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
+import { LipBalmComponent } from './lip-balm/lip-balm.component';
 import { LipsticksComponent } from './lipsticks/lipsticks.component';
+import { MoisturizerComponent } from './moisturizer/moisturizer.component';
 import { ProductsComponent } from './products/products.component';
+import { SunScreenComponent } from './sunscreen/sunscreen.component';
+import { TonerComponent } from './toner/toner.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,13 @@ import { ProductsComponent } from './products/products.component';
     DashboardCarouselComponent,
     ProductsComponent,
     BrushesComponent,
-    LipsticksComponent
+    LipsticksComponent,
+    CleanserComponent,
+    HairCareComponent,
+    LipBalmComponent,
+    TonerComponent,
+    SunScreenComponent,
+    MoisturizerComponent
   ],
   imports: [
     BrowserModule,
