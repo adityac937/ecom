ba07f3b20d33796ae176f8d4431089618fee7913
aditya chaudhari <aditya.ch@keka.com>
2021-11-14 22:50:18 +0530

    final-changes

--- src/app/lipsticks/lipsticks.component.ts
+++ src/app/lipsticks/lipsticks.component.ts
@@ -7,16 +7,45 @@ import { Component } from '@angular/core';
 export class LipsticksComponent {
   lipsticks = [
     {
-        name:"Lipstick 1",
-        img:"../../assets/images/products/lipsticks/1 (1).jpg",
+      name: "Clinique",
+      img: "../../assets/images/lipsticks/1 (1).jpg",
+      price: "390",
+      off: "10"
+    }, {
+      name: "Becca",
+      img: "../../assets/images/lipsticks/1 (2).jpg",
+      price: "490",
+      off: "12"
+    }, {
+      name: "Red",
+      img: "../../assets/images/lipsticks/1 (3).jpg",
+      price: "375",
+      off: "50"
+    }, {
+      name: "Black Gen",
+      img: "../../assets/images/lipsticks/1 (4).jpg",
+      price: "220",
+      off: "17"
+    }, {
+      name: "Gussi",
+      img: "../../assets/images/lipsticks/1 (5).jpg",
+      price: "390",
+      off: "33"
+    }, {
+      name: "Royale",
+      img: "../../assets/images/lipsticks/1 (6).jpg",
+      price: "275",
+      off: "5"
+    }, {
+      name: "Lancome",
+      img: "../../assets/images/lipsticks/1 (7).jpg",
+      price: "199",
+      off: "44"
+    }, {
+      name: "Pink Lips",
+      img: "../../assets/images/lipsticks/1 (8).jpg",
+      price: "122",
+      off: "3"
     },
-    {
-        name:"Lipstick 2",
-        img:"../../assets/images/products/lipsticks/1 (2).jpg",
-    },
-    {
-        name:"Lipstick 3",
-        img:"../../assets/images/products/lipsticks/1 (3).jpg",
-    }
-]
+  ]
 }
